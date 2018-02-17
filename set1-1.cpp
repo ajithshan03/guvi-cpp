996a6ec6717f3bd7224c808f750cf1177145bc54
ajithshan03 <ajithshan03@gmail.com>
2018-02-17 16:07:47 +0530

    program to Check whether a number is positive or negative or zero.

---
+++ set1-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    if(n == 0){
+        cout<<"The given number is zero";
+        }else if(n < 0){
+            cout<<"The given number is negative";
+            }else{
+                cout<<"The given number is positive";
+                }
+}
+
+
+
+
