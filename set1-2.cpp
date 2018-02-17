9de54d801ccf8be05ef5c8f64b42e5fcafe265f9
ajithshan03 <ajithshan03@gmail.com>
2018-02-17 16:27:19 +0530

    Create set1-2.cpp

---
+++ set1-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    if(n%2 == 0){
+        cout<<"The given number is a even number";
+        }else{
+                cout<<"The given number is a odd number";
+                }
+}
+
