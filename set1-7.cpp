cf74829679c2b4fe36bcc3c037f87d23015c7d1f
ajithshan03 <ajithshan03@gmail.com>
2018-02-27 17:42:44 +0530

    Write a program to print 'Hello' word N times given N

---
+++ set1-7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter number of times to print 'hello'..."<<endl;
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        cout<<"hello"<<endl;
+    }   
+}
