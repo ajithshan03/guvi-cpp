5d10c1b9aba59cbc645c1a1d86a8b795fddbada2
ajithshan03 <ajithshan03@gmail.com>
2018-02-28 19:08:01 +0530

    Display even numbers between two intervals.

---
+++ set-2/set2-15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,k;
+    cout<<"Enter the starting number"<<endl;
+    cin>>i;
+    cout<<"Enter the ending number"<<endl;
+    cin>>j;
+    cout<<"The even numbers between "<<i<<" and "<<j<<" are..."<<endl;
+    for(k = i+1;k < j;k++){
+        if(k%2 == 0){
+            cout<<k<<" ";
+        }
+    }
+    return 0;
+}
