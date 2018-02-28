b0a27bb44caffb249e8a807002fd7c11dfa76e6c
ajithshan03 <ajithshan03@gmail.com>
2018-02-28 12:43:49 +0530

    Calculate the power of a number given a number and the power.

---
+++ set-2/set2-12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,temp,rem,rev_num = 0;
+    cout<<"Enter a number"<<endl;
+    cin>>num;
+    temp = num;
+    while(temp != 0){
+        rem = temp % 10;
+        rev_num = rev_num*10 + rem;
+        temp /= 10;
+    }
+    if(rev_num == num){
+        cout<<"Its a palindrome"<<endl;
+    }else{
+        cout<<"Its not a palindrome"<<endl;
+    }
+
+    return 0;
+}
+
+
