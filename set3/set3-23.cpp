e828c8dcddbc8a01cec9b13401fe1b3a38f5683b
ajithshan03 <ajithshan03@gmail.com>
2018-03-03 21:49:07 +0530

    Given a number N and an array of N integers,Print the minimum element

--- set3/set3-23.cpp
+++ set3/set3-23.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main()
 {
-    int i,m,n[i],max;
+    int i,m,n[i],min;
     cout<<"Enter the size of the array"<<endl;
     cin>>m;
     cout<<"Enter the elements to the array"<<endl;
     for(i=0;i<m;i++){
         cin>>n[i];
     }
-    max = n[0];
+    min = n[0];
     for(i=1;i<m;i++){
-        if(max>n[i]){
-            max = n[i];
+        if(min>n[i]){
+            min = n[i];
         }
     }
-    cout<<"smallest number in the given list is:"<<max;
+    cout<<"smallest number in the given list is:"<<min;
 
     return 0;
 }
