2d8483f7d68db08194fdeac909b46c7be616158a
ajithshan03 <ajithshan03@gmail.com>
2018-03-06 22:55:18 +0530

    Given a sentence count the number of characters in given (without counting the space).

---
+++ set4/set4-31.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    int count = 0,i;
+    cout << "Enter a sentance: ";
+    getline(cin, str);
+    for(i = 0;i <= str.length()-1;i++){
+        if(str[i] == ' '){
+            continue;
+        }else{
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
