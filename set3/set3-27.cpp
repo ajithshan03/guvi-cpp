8784773fa34e37eb9b216b643e06d6fb88164bf6
ajithshan03 <ajithshan03@gmail.com>
2018-03-06 18:01:32 +0530

    Update set3-27.cpp

--- set3/set3-27.cpp
+++ set3/set3-27.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main(){
 string a;
-int set = 0;
-int i,temp;
+int set = 1;
 cin>>a;
-for(i = 0;a[i <= a.length() ;i++){
-    if(a[i] >= 0 && a[i] <= 9){
-        set = 1;
-    }else{
+for(int i = 0;i <= a.length()-1;i++){
+    if(a[i] >= '0' && a[i] <= '9'){
         set = 0;
+    }else{
+        set = 1;
         break;
     }
 }
 
-if(set == 1){
+if(set == 0){
     cout<<"The string is numeric"<<endl; 
 }else{
     cout<<"The string is non-numeric"<<endl; 
 }
 return 0;
 }
+
