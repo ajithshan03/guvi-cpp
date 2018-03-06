b290ba2e653dd310dbe9f8e4a01a8ba6aa13752c
ajithshan03 <ajithshan03@gmail.com>
2018-03-06 23:04:17 +0530

    Count the number of numeric characters in a given string

---
+++ set4/set4-35.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    int count = 0,i;
+    cout << "Enter a sentance: ";
+    getline(cin, str);
+    for(i = 0;i <= str.length()-1;i++){
+        if(str[i] >= '0' && str[i] <= '9'){
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
