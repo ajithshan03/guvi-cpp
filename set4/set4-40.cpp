c61593ab6cd66e2ad9e728ab88b328f1807cd2b6
ajithshan03 <ajithshan03@gmail.com>
2018-03-07 07:26:17 +0530

    Given a number N find print fibonacci series of N numbers

--- set4/set4-40.cpp
+++ set4/set4-40.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main()
 {
-    int i,a = 0,b = 1;
+    int i,a = 0,b = 1,ans;
     cout<<"Enter a number:";
     cin>>i;
-    if( i == 1){
-        cout<<a<<endl;
-    }else if(i == 2){
-        cout<<b<<endl;
-    }else{
-        for(int j = 1;j <= i;j++){
+    cout<<"Fibonacci series upto "<<i<<" :";
+    for(int j = 1;j <= i;j++){
+        if( j == 1){
+            cout<<a;
+        }else if(j == 2){
+            cout<<" "<<b;
+        }else {
             ans = a + b;
-            
+            a = b;
+            b = ans;
             cout<<" "<<ans;
         }
     }
-            
-
     return 0;
 }
