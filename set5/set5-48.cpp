59d50d98c6f39d0f458f10c1009d46fc4ddfc3fb
ajithshan03 <ajithshan03@gmail.com>
2018-03-07 21:57:25 +0530

    Given a number N followed by N numbers print the average(integer) of the N numbers.

---
+++ set5/set5-48.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,a[n],result = 0;
+    cout<<"Enter a number:";
+    cin>>n;
+    for(int j = 0;j < n;j++){
+        cin>>a[j];
+        result += a[j];
+    }
+    result = result/n;
+    cout<<"Average of the array is :"<<result;
+    return 0;
+}
