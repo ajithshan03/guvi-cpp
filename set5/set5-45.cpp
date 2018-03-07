bf12e75df162afba584d0c438cb391bbd9fbeaa4
ajithshan03 <ajithshan03@gmail.com>
2018-03-07 21:02:27 +0530

    Count the number of numeric digits in a number given

---
+++ set5/set5-45.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<<"Enter the number: ";
+    cin>>str;
+    cout<<"Number of digits :"<<str.length();
+    return 0;
+}
