04e61d0dff7ce9af5e3d602db7c81fd058522767
ajithshan03 <ajithshan03@gmail.com>
2018-03-08 07:32:53 +0530

    Given a number N,print it in words

---
+++ set6/set6-52.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"Enter a number:";
+    cin>>i;
+    switch(i){
+        case 1:
+        cout<<"One";
+        break;    
+        case 2:
+        cout<<"Two";
+        break;
+        case 3:
+        cout<<"Three";
+        break;
+        case 4:
+        cout<<"Four";
+        break;
+        case 5:
+        cout<<"Five";
+        break;
+        case 6:
+        cout<<"six";
+        break;
+        case 7:
+        cout<<"seven";
+        break;
+        case 8:
+        cout<<"eight";
+        break;
+        case 9:
+        cout<<"nine";
+        break;
+        case 10:
+        cout<<"Ten";
+        break;
+        default:
+        cout<<"Enter a number between 1 and 10";
+    }
+    return 0;
+}
