4d21429b4e2d5a59447ea687b274ff825162b2b4
ajithshan03 <ajithshan03@gmail.com>
2018-03-17 14:02:09 +0530

    Given a number N find whether it is a multiple of 7.

---
+++ set7/set7-68.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin>>i;
+    if(i%7 == 0){
+        cout<<"yes";
+    } else {
+        cout<<"no";
+    }
+    return 0;
+}
