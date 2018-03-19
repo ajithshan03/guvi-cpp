b8a0ad0110243bb9b147017116d66c93ebbfb17e
ajithshan03 <ajithshan03@gmail.com>
2018-03-19 22:04:28 +0530

    Given a number N find whether it is a multiple of 13.

---
+++ set8/set8-78.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    if(i % 13 == 0){
+        cout<<"Yes";
+    } else {
+        cout<<"No";
+    }
+    return 0;
+}
