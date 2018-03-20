75451c3b0119cbb354bebefad9a44079cd42e4f0
ajithshan03 <ajithshan03@gmail.com>
2018-03-20 10:57:23 +0530

    Write a program to compare two strings without using strcmp() function.print the greater string or if equal print the string once

--- set5/set5-42.cpp
+++ set5/set5-42.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    string str1,str2;
-    cout<<"Enter the two strings"<<endl;
-    cin>>str1>>str2;
-    if(str1.length() == str2.length()){
+  string str1,str2;
+  int set=0;
+  cout<<"enter the strings :";
+  cin>>str1>>str2;
+  for(int i=0; i <= str1.length()-1;i++){
+      if(str1[i] == str2[i]){
+          set = 1;
+      }else if(str1[i] > str2[i]){
+          cout<<str1;
+          set = 0;
+          break;
+      }else{
         cout<<str2;
-    }else if(str1.length() > str2.length()){
-        cout<<"Greater string is:"<<str1;
-    }else{
-        cout<<"Greater string is:"<<str2;
+        set = 0;
+        break;
     }
-    return 0;
+  }
+  if(set == 1){
+      cout<<str1<<" "<<str2;
+  }
 }
