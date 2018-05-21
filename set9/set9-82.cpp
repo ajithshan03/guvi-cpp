bc0263d787451c68c33c5b30793d0c4b4661ab49
Ajith Shan <ajithshan03@gmail.com>
2018-05-21 10:37:54 +0530

    Given length(L) and breadth(B) of a farm,print the area of the farm.(print the answer upto 5 decimal places)

---
+++ set9/set9-82.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the two numbers A and B";
+    cin>>a>>b;
+    int area = a*b;
+    cout<<"Area of the farm is :"<<area;
+
+    return 0;
+}
