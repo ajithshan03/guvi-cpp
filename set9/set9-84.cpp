5b4ac7ef02c30b183c0845a4f5ab596a19e4be98
Ajith Shan <ajithshan03@gmail.com>
2018-05-21 12:01:06 +0530

    Given a character Ch,print its ASCII value.

---
+++ set9/set9-84.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    char Ch;
+    cout<<"Enter the letter to find its ASCII code :";
+    cin>>Ch;
+    cout<<int(Ch);
+    return 0;
+}
